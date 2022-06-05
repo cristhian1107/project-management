3ff50198893982c60c0bcff268b754c2121c4738
cristhian1107 <cristhian.cjaa@gmail.com>
2022-06-05 18:06:52 -0500

    Feat (Databse) Create procedures

--- pm_database/0-tables.sql
+++ pm_database/0-tables.sql
@@ -7,8 +7,14 @@
 -- Update at:
 -- ***************************************************
 CREATE DATABASE IF NOT EXISTS project_management;
-USE project_management;
 
+CREATE USER IF NOT EXISTS 'development'@'localhost';
+SET PASSWORD FOR 'development'@'localhost' = 'dev_pwd(001)';
+GRANT ALL ON project_management.* TO 'development'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'development'@'localhost';
+FLUSH PRIVILEGES;
+
+USE project_management;
 
 -- ************************************************ --
 -- ********************* TABLES ******************* --
@@ -18,17 +24,35 @@ USE project_management;
 -- Create at: 2022-06-04 (Cristhian Apaza)
 -- Update at:
 -- ***************************************************
-CREATE TABLE `companies`
+CREATE TABLE IF NOT EXISTS `companies`
 (
  `id`        bigint NOT NULL ,
  `ruc`       varchar(25) NOT NULL ,
  `name`      varchar(50) NOT NULL ,
  `tradename` varchar(50) NULL ,
  `address`   varchar(100) NULL ,
- `create_at` timestamp NOT NULL ,
+ `create_at` datetime NOT NULL ,
  `create_by` varchar(50) NOT NULL ,
- `update_at` timestamp NULL ,
+ `update_at` datetime NULL ,
  `update_by` varchar(100) NULL ,
+
+PRIMARY KEY (`id`)
+);
+
+-- ************ `roles` ******************************
+-- Create at: 2022-06-04 (Cristhian Apaza)
+-- Update at:
+-- ***************************************************
+CREATE TABLE IF NOT EXISTS `roles`
+(
+ `id`          bigint NOT NULL ,
+ `name`        varchar(50) NOT NULL ,
+ `description` varchar(150) NULL ,
+ `is_active`   bit NOT NULL ,
+ `create_at`   datetime NOT NULL ,
+ `create_by`   varchar(50) NOT NULL ,
+ `update_at`   datetime NULL ,
+ `update_by`   varchar(100) NULL ,
 
 PRIMARY KEY (`id`)
 );
@@ -37,16 +61,16 @@ PRIMARY KEY (`id`)
 -- Create at: 2022-06-04 (Cristhian Apaza)
 -- Update at:
 -- ***************************************************
-CREATE TABLE `options`
+CREATE TABLE IF NOT EXISTS `options`
 (
  `id`          bigint NOT NULL ,
  `name`        varchar(50) NOT NULL ,
  `alias`       varchar(25) NOT NULL ,
  `description` varchar(150) NULL ,
  `is_active`   bit NOT NULL ,
- `create_at`   timestamp NOT NULL ,
+ `create_at`   datetime NOT NULL ,
  `create_by`   varchar(50) NOT NULL ,
- `update_at`   timestamp NULL ,
+ `update_at`   datetime NULL ,
  `update_by`   varchar(100) NULL ,
 
 PRIMARY KEY (`id`)
@@ -56,7 +80,7 @@ PRIMARY KEY (`id`)
 -- Create at: 2022-06-04 (Cristhian Apaza)
 -- Update at:
 -- ***************************************************
-CREATE TABLE `users`
+CREATE TABLE IF NOT EXISTS `users`
 (
  `id`         bigint NOT NULL ,
  `company_id` bigint NOT NULL ,
@@ -64,19 +88,44 @@ CREATE TABLE `users`
  `name`       varchar(50) NOT NULL ,
  `lastname`   varchar(50) NOT NULL ,
  `email`      varchar(50) NOT NULL ,
+ `user`       varchar(50) NOT NULL ,
+ `password`   varchar(50) NOT NULL ,
  `gender`     char(1) NOT NULL ,
  `position`   varchar(50) NULL ,
  `department` varchar(50) NULL ,
  `campus`     varchar(50) NOT NULL ,
- `create_at`  timestamp NOT NULL ,
+ `create_at`  datetime NOT NULL ,
  `create_by`  varchar(50) NOT NULL ,
- `update_at`  timestamp NULL ,
+ `update_at`  datetime NULL ,
  `update_by`  varchar(50) NULL ,
 
+
 PRIMARY KEY (`id`),
+UNIQUE KEY `username` (`user`),
 KEY `company_id` (`company_id`),
 CONSTRAINT `fk_companies_users` FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`),
 KEY `role_id` (`role_id`),
 CONSTRAINT `fk_roles_users` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
 );
 
+
+-- ************ `roles_options` **********************
+-- Create at: 2022-06-04 (Cristhian Apaza)
+-- Update at:
+-- ***************************************************
+CREATE TABLE IF NOT EXISTS `roles_options`
+(
+ `role_id`   bigint NOT NULL ,
+ `option_id` bigint NOT NULL ,
+ `is_active` bit NOT NULL ,
+ `create_at` datetime NOT NULL ,
+ `create_by` varchar(50) NOT NULL ,
+ `update_at` datetime NULL ,
+ `update_by` varchar(50) NULL ,
+
+PRIMARY KEY (`role_id`, `option_id`),
+KEY `role_id` (`role_id`),
+CONSTRAINT `FK_128` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
+KEY `option_id` (`option_id`),
+CONSTRAINT `FK_131` FOREIGN KEY (`option_id`) REFERENCES `options` (`id`)
+);
---
+++ pm_database/1-procedures.sql
@@ -0,0 +1,128 @@
+DROP PROCEDURE IF EXISTS users_all;
+-- =========================================================
+-- Autor - Fecha Crea  : Cristhian Apaza - 2022-06-05
+-- Descripcion         : Select all table
+-- Autor - Fecha Modif :
+-- Descripcion         :
+-- =========================================================
+DELIMITER $$
+CREATE PROCEDURE users_all()
+BEGIN
+
+    SELECT
+          id , company_id , role_id , name
+        , lastname , email , user , password
+        , gender , position , department , campus
+        , create_at , create_by , update_at , update_by
+    FROM users;
+END $$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS users_one;
+-- =========================================================
+-- Autor - Fecha Crea  : Cristhian Apaza - 2022-06-05
+-- Descripcion         : select a record from the table
+-- Autor - Fecha Modif :
+-- Descripcion         :
+-- =========================================================
+DELIMITER $$
+CREATE PROCEDURE users_one
+( IN pbigid bigint )
+BEGIN
+
+    SELECT id , company_id , role_id , name
+        , lastname , email , user , password
+        , gender , position , department , campus
+        , create_at , create_by , update_at , update_by
+     FROM users
+    WHERE id = pbigid;
+
+END $$
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS users_insert;
+-- =========================================================
+-- Autor - Fecha Crea  : Cristhian Apaza - 2022-06-05
+-- Descripcion         : Insert new record from the table
+-- Autor - Fecha Modif :
+-- Descripcion         :
+-- =========================================================
+DELIMITER $$
+CREATE PROCEDURE users_insert
+( INOUT pbigid bigint
+, IN pbigcompany_id bigint
+, IN pbigrole_id bigint
+, IN pvchname varchar(50)
+, IN pvchlastname varchar(50)
+, IN pvchemail varchar(50)
+, IN pvchuserr varchar(50)
+, IN pvchpassword varchar(50)
+, IN pchrgender char(1)
+, IN pvchposition varchar(50)
+, IN pvchdepartment varchar(50)
+, IN pvchcampus varchar(50)
+, IN pdtmcreate_at datetime
+, IN pvchcreate_by varchar(50)
+, IN pdtmupdate_at datetime
+, IN pvchupdate_by varchar(50) )
+BEGIN
+
+    INSERT INTO users
+        ( id , company_id , role_id , name
+        , lastname , email , userr , password
+        , gender , position , department , campus
+        , create_at , create_by , update_at , update_by)
+    VALUES
+        ( pbigid , pbigcompany_id , pbigrole_id , pvchname
+        , pvchlastname , pvchemail , pvchuserr , pvchpassword
+        , pchrgender , pvchposition , pvchdepartment , pvchcampus
+        , pdtmcreate_at , pvchcreate_by , pdtmupdate_at , pvchupdate_by);
+END $$
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS users_update;
+-- =========================================================
+-- Autor - Fecha Crea  : Cristhian Apaza - 2022-06-05
+-- Descripcion         : Update record from the table
+-- Autor - Fecha Modif :
+-- Descripcion         :
+-- =========================================================
+DELIMITER $$
+CREATE PROCEDURE users_update
+( IN pbigid bigint
+, IN pbigcompany_id bigint
+, IN pbigrole_id bigint
+, IN pvchname varchar(50)
+, IN pvchlastname varchar(50)
+, IN pvchemail varchar(50)
+, IN pvchuserr varchar(50)
+, IN pvchpassword varchar(50)
+, IN pchrgender char(1)
+, IN pvchposition varchar(50)
+, IN pvchdepartment varchar(50)
+, IN pvchcampus varchar(50)
+, IN pdtmcreate_at datetime
+, IN pvchcreate_by varchar(50)
+, IN pdtmupdate_at datetime
+, IN pvchupdate_by varchar(50) )
+BEGIN
+
+    UPDATE users
+      SET company_id = pbigcompany_id
+        , role_id = pbigrole_id
+        , name = pvchname
+        , lastname = pvchlastname
+        , email = pvchemail
+        , userr = pvchuserr
+        , password = pvchpassword
+        , gender = pchrgender
+        , position = pvchposition
+        , department = pvchdepartment
+        , campus = pvchcampus
+        , update_at = pdtmupdate_at
+        , update_by = pvchupdate_by
+    WHERE id = pbigid;
+END $$
+DELIMITER ;
