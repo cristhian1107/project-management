eb863c5aa97c945ff8cfd7d15b39640c3124d16e
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-06-06 15:29:34 -0500

    Feat(server.py): update

--- pm_backend/server.py
+++ pm_backend/server.py
@@ -4,6 +4,7 @@ from os import getenv
 from flask import Flask, jsonify
 from flask_cors import CORS
 from database.db_user import DBUser
+from database import storage
 from routes import app_views
 from flasgger import Swagger
 from flasgger.utils import swag_from
@@ -17,14 +18,14 @@ cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 @app.teardown_appcontext
 def teardown(self):
     """Removes the current SQLAlchemy Session"""
-    return storage.close()
+    return storage.close_db()
 
 
 @app.errorhandler(404)
 def error(e):
     """Handler for 404 errors"""
     return jsonify({"error": "Not found"}), 404
-print(DBUser.login(hola, mundo))
+#print(DBUser.login(hola, mundo))
 
 if __name__ == '__main__':
     host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
