3f3fc2de9690efa31d7573d4073b72950270cd34
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-06-06 21:38:08 -0500

    Feat(routes/user.py): add make_response

--- pm_backend/routes/user.py
+++ pm_backend/routes/user.py
@@ -3,7 +3,7 @@
 from database import storage
 from models.user import User
 from routes import app_views
-from flask import jsonify, abort, request
+from flask import jsonify, abort, request, make_response
 from flasgger.utils import swag_from
 from database.db_procedure import DBProcedures
 
@@ -20,5 +20,5 @@ def password():
     """
     user = DBProcedures.users_login('javier.pilco', 'ja-pi')
     if user == {}:
-        return jsonify({'status': 'failure'}), 401 # RFC 7235
-    return jsonify(user.to_dict()), 201
+        return make_response(jsonify({'status': 'failure'}), 401) # RFC 7235
+    return make_response(jsonify(user.to_dict()), 201)
