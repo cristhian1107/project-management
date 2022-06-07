de28faa3410d1ebd2035709cc214392c9a4bb5ae
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-06-06 21:38:06 -0500

    Feat(routes/index.py): add make_response

--- pm_backend/routes/index.py
+++ pm_backend/routes/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module implement a rule that returns the status of the application"""
-from flask import jsonify
+from flask import jsonify, make_response
 from database import storage
 from routes import app_views
 from models.user import User
@@ -9,12 +9,12 @@ from models.user import User
 @app_views.route("/status", strict_slashes=False)
 def view_status():
     """View function that return a json message"""
-    return jsonify({"status": "OK"})
+    return make_response(jsonify({"status": "OK"}), 200)
 
 
 @app_views.route("/stats", strict_slashes=False)
 def view_stats():
     """Veiw function that retrieves the number of each object by type"""
-    return jsonify({
+    return make_response(jsonify({
         "users": storage.count(User)
-    })
+    }), 200)
