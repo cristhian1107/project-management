cdc4ec7f46931e265448e58f9a1f67c36d6ea114
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-06-07 18:03:06 -0500

    Feat(Backend): code was updated with test

--- pm_backend/routes/user.py
+++ pm_backend/routes/user.py
@@ -8,19 +8,24 @@ from flasgger.utils import swag_from
 from database.db_procedure import DBProcedures
 import jwt
 
-@app_views.route('/login', methods=['POST', 'GET'],
+@app_views.route('/login', methods=['POST'],
                  strict_slashes=False)
 def password():
     """Validate login"""
-    """
+
     body = request.get_json()
-    name = body.get('user')
+    name = body.get('username')
     password = body.get('password')
-    user = DBProcedures.users_login(name, password)
-    """
+    
+    #user = DBProcedures.users_login(name, password)
+
     user = DBProcedures.users_login('javier.pilco', 'ja-pi')
     if user == {}:
         return make_response(jsonify({'status': 'failure'}), 401) # RFC 7235
-    encoded_jwt = jwt.encode({"some": user.user}, "secret", algorithm="HS256")
-    setattr(user, 'Tokencillo', str(encoded_jwt))
+    del user.role
+    encoded_jwt = jwt.encode({"username": user.user, "id": user.id}, "secret",algorithm="HS256")
+    setattr(user, 'jwt', str(encoded_jwt))
     return make_response(jsonify(user.to_dict()), 201)
+#return make_response(jsonify({"jwt":"jwt"}), 201)
+#    res = make_response(jsonify(user.to_dict()), 201)
+#    return res.set_cookie("thiscookie", "cookie")
