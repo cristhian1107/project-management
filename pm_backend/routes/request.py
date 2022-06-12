51a45b2520665b1a9032733d64d54c1d60648b7e
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-06-12 00:24:31 -0500

    Feat(request.py): request post was updated

--- pm_backend/routes/request.py
+++ pm_backend/routes/request.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """route for request"""
-from database import storage
+from database import storage, tables
 from models.user import User
 from models.request import Request
 from routes import app_views
@@ -8,11 +8,15 @@ from flask import jsonify, abort, request, make_response
 from flasgger.utils import swag_from
 from database.db_procedure import DBProcedures
 import jwt
-
+from datetime import datetime
+time = '%Y-%m-%dT%H:%M:%S.%f'
+"""
 @app_views.route('/allrequest', methods=['GET'],
                  strict_slashes=False)
 def all_request(date):
-    """returns list of all projects"""
+    """
+#returns list of all projects
+"""
     start = request.args.get('start', None)
     end = request.args.get('end', None)
     state = request.args.get('state', None)
@@ -33,11 +37,14 @@ def all_request(date):
         if create_at in date:
             new_list+ = i 
     return make_response(jsonify(new_list.to_dict()), 201)
-
+"""
+"""
 @app_views.route('/request', methods=['GET'],
                  strict_slashes=False)
 def get_request(date):
-    """returns a list of specific projects"""
+    """
+#returns a list of specific projects
+"""
     start = request.args.get('start', None)
     end = request.args.get('end', None)
     state = request.args.get('state', None)
@@ -51,23 +58,27 @@ def get_request(date):
     }
     # Necesito una lista de diccionarios de todos los proyectos con todos sus datos
     res = DBProcedures.users_login('javier.pilco', 'ja-pi')
-
+"""
     
 @app_views.route('/request', methods=['POST'],
                  strict_slashes=False)
-def insert_request(date):
+def insert_request():
     """inserts a new requirement/project"""
     item = Request()
-    item.date_issue = request.args.get('date_issue', None)
+    data = request.get_json()
+    item.date_issue = datetime.strptime(data.get('date_issue', None), time)
 #    item.date_tentative = request.args.get('date_tentative', None)
-    item.user_id = request.args.get('user_id', None)
-    item.reason = request.args.get('reason', None)
-    item.subject = request.args.get('subject', None)
+    item.user_id = data.get('user_id', None)
+    item.reason = data.get('reason', None)
+    item.subject = data.get('subject', None)
     item.table_pri = tables.get('PRI')
-    item.code_pri = request.args.get('code_pri', None)
-
+    item.code_pri = data.get('code_pri', None)
+    item.code = ''
+    item.percentage = 0
     # Data is sent to procedures and is returned on success or failure.
-    res = DBProcedures.request_insert(item)
+    res = DBProcedures.requests_insert(item)
+    
+    print(item.to_list())
     if res == None:
-        return make_response(jsonify({'request': 'failure'}.to_dict()), 204)
-    return make_response(jsonify({'request': 'success'}.to_dict()), 201)
+        return make_response(jsonify({'request': 'failure'}), 204)
+    return make_response(jsonify({'request': 'success'}), 201)
