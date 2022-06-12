397a1a6aef9eb3ff71fb60f1737ac7ecbd52991b
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-06-12 15:36:20 -0500

    Feat(request.py): all_request and get_request method was updated

--- pm_backend/routes/request.py
+++ pm_backend/routes/request.py
@@ -10,56 +10,30 @@ from database.db_procedure import DBProcedures
 import jwt
 from datetime import datetime
 time = '%Y-%m-%dT%H:%M:%S.%f'
-"""
+
+
 @app_views.route('/allrequest', methods=['GET'],
                  strict_slashes=False)
 def all_request(date):
-    """
-#returns list of all projects
-"""
+    """returns list of all projects"""
     start = request.args.get('start', None)
     end = request.args.get('end', None)
-    state = request.args.get('state', None)
-    id = request.args.get('id', None)
-    code = request.args.get('codigo', None)
-    data = {
-        'start': start,
-        'end': end,
-        'id': id,
-        'code': code,
-    }
-    # Necesito una lista de diccionarios de todos los proyectos con todos sus datos
-    res = DBProcedures.users_login('javier.pilco', 'ja-pi')
+    # Necesito una lista de diccionarios de todos los proyectos con
+    # todos sus datos en ese rango de fecha
+    res = DBProcedures.request_all(start, end)
+    make_response(jsonify(res.to_dict()), 200)
 
-    new_list = []
-    for i in list_data:
-        create_at = i.get('create_at')
-        if create_at in date:
-            new_list+ = i 
-    return make_response(jsonify(new_list.to_dict()), 201)
-"""
-"""
+
 @app_views.route('/request', methods=['GET'],
                  strict_slashes=False)
 def get_request(date):
-    """
-#returns a list of specific projects
-"""
-    start = request.args.get('start', None)
-    end = request.args.get('end', None)
-    state = request.args.get('state', None)
+    """returns a list of specific projects"""
     id = request.args.get('id', None)
-    code = request.args.get('codigo', None)
-    data = {
-        'start': start,
-        'end': end,
-        'id': id,
-        'code': code,
-    }
-    # Necesito una lista de diccionarios de todos los proyectos con todos sus datos
-    res = DBProcedures.users_login('javier.pilco', 'ja-pi')
-"""
-    
+    # Necesito un diccionario de el proyectos con todos sus datos
+    res = DBProcedures.request_one(id)
+    return make_response(jsonify(res.to_dict()), 200)
+
+
 @app_views.route('/request', methods=['POST'],
                  strict_slashes=False)
 def insert_request():
@@ -67,7 +41,7 @@ def insert_request():
     item = Request()
     data = request.get_json()
     item.date_issue = datetime.strptime(data.get('date_issue', None), time)
-#    item.date_tentative = request.args.get('date_tentative', None)
+    # item.date_tentative = request.args.get('date_tentative', None)
     item.user_id = data.get('user_id', None)
     item.reason = data.get('reason', None)
     item.subject = data.get('subject', None)
@@ -77,8 +51,8 @@ def insert_request():
     item.percentage = 0
     # Data is sent to procedures and is returned on success or failure.
     res = DBProcedures.requests_insert(item)
-    
+
     print(item.to_list())
-    if res == None:
+    if res is None:
         return make_response(jsonify({'request': 'failure'}), 204)
     return make_response(jsonify({'request': 'success'}), 201)
