02457dc7c84c8beeb364039700ac907ca7e57dc1
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-06-13 17:09:19 -0500

    Feat(request.py): update

--- pm_backend/routes/request.py
+++ pm_backend/routes/request.py
@@ -3,6 +3,7 @@
 from database import storage, tables
 from models.user import User
 from models.request import Request
+from models.request_event import RequestEvent
 from routes import app_views
 from flask import jsonify, abort, request, make_response
 from flasgger.utils import swag_from
@@ -38,6 +39,7 @@ def get_request():
     id = request.args.get('id', None)
     # Necesito un diccionario de el proyectos con todos sus datos
     res = DBProcedures.requests_one(id)
+    print(res)
     if res is None:
         return make_response(jsonify({'request': 'empty'}), 204)
     return make_response(jsonify(res.to_dict()), 200)
@@ -60,8 +62,7 @@ def insert_request():
     item.percentage = 0
     # Data is sent to procedures and is returned on success or failure.
     res = DBProcedures.requests_insert(item)
-
-    print(item.to_list())
+    print(res)
     if res is None:
         return make_response(jsonify({'request': 'failure'}), 204)
     return make_response(jsonify({'request': 'success'}), 201)
@@ -85,26 +86,24 @@ def update_request():
     item.table_pri = tables.get('PRI')
     item.code_typ = data.get('code_typ', None)
     item.code_pri = data.get('code_pri', None)
-    print("************************************")
-    print(item.to_list())
-    print("************************************")
-    print(item.to_dict(True))
-    print("************************************")
     res = DBProcedures.requests_update(item)
     if res is None:
         return make_response(jsonify({'request': 'failure'}), 204)
     return make_response(jsonify({'request': 'success'}), 201)
 
 
-@app_views.route('/state', methods=['get'],
+@app_views.route('/event', methods=['POST'],
                  strict_slashes=False)
-def update_state():
+def update_event():
     """changes state"""
-    """
-    enviar cambio de estado a procedure
-    iduser
-    id requereimineto
-    id estado
-    res = DBProcedures.requests_state(item)
-    """
+    data = request.get_json()
+    item = RequestEvent()
+    item.project_id = data.get('project_id', None)
+    item.code_sta = data.get('code_sta', None)
+    item.tabe_sta = tables.get('STA')
+    item.date_issue = data.get('date_issue', None)
+    item.user_id = data.get('user_id', None)
+    res = DBProcedures.requests_events_insert(item)
+    if res is None:
+        return make_response(jsonify({'request': 'failure'}), 204)
     return make_response(jsonify({'request': 'success'}), 201)
