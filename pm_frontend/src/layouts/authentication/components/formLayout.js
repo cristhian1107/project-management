ddc6d01842b897fbd6125bab4d38b997057f33a9
MiguelBarreraDev <barreram987@gmail.com>
2022-06-20 06:01:16 -0500

    Update (frontend): Login view

--- pm_frontend/src/layouts/authentication/components/formLayout.js
+++ pm_frontend/src/layouts/authentication/components/formLayout.js
@@ -4,24 +4,61 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 
-export default function FormLayout ({ titleForm, children }) {
+export default function FormLayout ({ children }) {
 
   return (
     <Box
       sx={{
-        mt: 8,
+        mt: 10,
         px: 2,
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
       }}
     >
-      <Avatar sx={{ m: 1, background: 'var(--btn-gradient)' }}>
-        <LockOutlinedIcon/>
-      </Avatar>
-      <Typography component="h1" variant="h5">
-        { titleForm ? titleForm : 'Sign in' }
-      </Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          width: 'fit-content',
+          position: 'relative',
+        }}
+      >
+        <Avatar
+          sx={{
+            m: 1,
+            background: 'transparent',
+            fontStyle: 'italic',
+            border: '1px dashed var(--btn-primary)',
+            width: 90,
+            height: 90,
+            fontSize: '1.7rem',
+            fontWeight: 'bold',
+          }}
+        >
+          <LockOutlinedIcon
+            sx={{
+              width: 40,
+              height: 40,
+              color: '#fff'
+            }}
+          />
+        </Avatar>
+        <Typography
+          component="h1"
+          variant="h5"
+          sx={{
+            position: 'absolute',
+            left: 85,
+            fontStyle: 'italic',
+            color: '#fff',
+            letterSpacing: 4,
+            fontSize: '1.5rem',
+          }}
+        >
+          LogIn
+        </Typography>
+      </Box>
       {children}
     </Box>
   );
--- pm_frontend/src/layouts/authentication/sign-in/basicLayout.js
+++ pm_frontend/src/layouts/authentication/sign-in/basicLayout.js
@@ -1,38 +1,44 @@
 // Styles @mui 
 import CssBaseline from '@mui/material/CssBaseline';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 // Images
-import bgImage from 'assets/images/back.jpeg';
+// import bgImage from 'assets/images/back.jpeg';
+import bgImage from 'assets/images/bgWindow.jpeg';
 
 export default function BasicLayout({children}) {
 
   return (
-    <Grid container component="main" sx={{ height: '100vh' }}>
+    <Grid
+      container component="main"
+      sx={{
+        position: 'relative',
+        height: '100vh',
+        background: `url(${bgImage})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundSize: '100vw 100vh',
+        justifyContent: 'center',
+        '&::before': {
+          content: '""',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          background: '#000a',
+          backdropFilter: 'blur(10px)',
+        }
+      }}
+    >
       <CssBaseline />
       <Grid
         item
-        xs={false}
-        sm={4}
-        md={7}
-        sx={{
-          // backgroundImage: 'url(https://source.unsplash.com/random)',
-          backgroundImage: `url(${bgImage})`,
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      />
-      <Grid
-        item
-        xs={12}
-        sm={8}
-        md={5}
-        component={Paper}
         elevation={6}
+        xs={12}
+        sm={6}
         sx={{
-          backgroundColor: (t) =>
-          t.palette.mode === 'light' ? 'var(--bg-white)' : t.palette.grey[900],
+          position: 'relative',
+          boxShadow: 'none',
           pt: 8,
         }}
       >
