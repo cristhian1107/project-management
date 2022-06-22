d9c34f9d873dd85a125d1438507d9402a612ef33
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-06-21 20:22:28 -0500

    update

--- pm_frontend/src/layouts/dashboard/index.js
+++ pm_frontend/src/layouts/dashboard/index.js
@@ -1,3 +1,14 @@
+import HeaderSection from 'layouts/dashboard/HeaderSection';
+
 export default function Dashboard() {
-  return <h1>Dashboard</h1>
+  return
+  <>
+  <h1>Dashboard</h1>
+  <HeaderSection />
+  </>
 }
+
+//import StadisticSection from 'layouts/solicitudes/components/filtersSection';
+//import TableSection from 'layouts/solicitudes/components/tableSection';
+//      <StadisticSection css={{ my: 10 }}/>
+//      <TableSection />
