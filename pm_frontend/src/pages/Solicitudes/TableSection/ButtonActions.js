5bd72b886d9d0c932ed02101260cb0375b3dd201
MiguelBarreraDev <barreram987@gmail.com>
2022-06-25 20:08:41 -0500

    Add (fronted): Styles for the modal that review a requests

--- pm_frontend/src/pages/Solicitudes/TableSection/ButtonActions.js
+++ pm_frontend/src/pages/Solicitudes/TableSection/ButtonActions.js
@@ -1,10 +1,12 @@
 // React
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 // @mui
 import Grid from '@mui/material/Grid'
 import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
 import InputAdornment from '@mui/material/InputAdornment';
+import MenuItem from '@mui/material/MenuItem';
+import Icon from '@mui/material/Icon';
 import EditIcon from '@mui/icons-material/Edit';
 import FindInPageIcon from '@mui/icons-material/FindInPage';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
@@ -18,10 +20,11 @@ import ButtonModal from 'components/ModalForm/ButtonModal';
 import TextFieldFullWidth from 'components/textFieldFullWidth';
 
 export default function ButtonActions ({ dataRequest }) {
+  const [counter, setCounter] = useState(0);
   const [open, setOpen] = useState(false);
 
   const handleClose = () => setOpen(false);
-  
+
   const handleOpen = () => setOpen(true);
 
   const handleConfirmation = (e) => {
@@ -53,6 +56,54 @@ export default function ButtonActions ({ dataRequest }) {
       </Grid>
     )
   }
+  
+  const LetterCounter = ({ current, total }={ current: 0, total: 0 }) => {
+    return (
+      <Icon
+        sx={{
+          width: 'fit-content',
+          height: 'fit-content',
+          fontSize: '1.2rem',
+        }}
+      >
+        {`${current}/${total}`}
+      </Icon>
+    )
+  }
+  const FormFieldItem = useCallback(({ bp, css, renderIcon, lines, ...props }) => {
+
+    return (
+      <Grid
+        item
+        {...bp}
+      >
+        <TextFieldFullWidth
+          css={{
+            background: props.disabled ? '#77a2': '#fff',
+            "& textarea" : {
+              minHeight: lines && props?.value ? 'fit-content' : 200,
+              maxHeight: lines && props?.value && 200,
+            },
+            ...css,
+          }}
+          variant="outlined"
+          multiline={lines?.multiline ? true : false}
+          {...props}
+          inputProps={{
+            maxLength: lines?.multiline ? lines?.maxLength : 'auto',
+          }}
+          InputProps={{
+            sx: { fontSize: { xs: '.85rem', sm:'inherit' } },
+            endAdornment: renderIcon && (
+              <InputAdornment position='end' sx={{ position: 'absolute', right: 10, alignSelf: 'end' }}>
+                {renderIcon}
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Grid>
+    )
+  }, []);
 
   return (
     <Grid container sx={{ justifyContent: 'center', flexWrap: 'nowrap' }}>
@@ -71,119 +122,81 @@ export default function ButtonActions ({ dataRequest }) {
           component="form"
           onSubmit= {handleConfirmation}
           sx={{
-            '& > :not(style)': { m: 1 },
+            // '& > :not(style)': { m: 1 },
+            gap: 2,
             justifyContent: 'space-between',
           }}
           // noValidate
           autoComplete="off"
         >
-          <Grid
-            item
-            xs={12}
+          <FormFieldItem
+            required
+            select
+            bp={{ xs: 5.5, sm: 3.5 }}
+            label='Tipo'
+            name='code_typ'
+            defaultValue=''
           >
-            <TextFieldFullWidth
-              label="Asunto"
-              name="subject"
-              variant="outlined"
-              value={dataRequest.subject}
-              InputProps={{
-                readOnly: true,
-                endAdornment: (
-                  <InputAdornment position='end'>
-                    <CheckCircleOutlineIcon />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid
-            item
-            xs={12}
+            <MenuItem key='requirement' value='requiremt'>Requerimiento</MenuItem>
+            <MenuItem key='project' value='project'>Proyecto</MenuItem>
+          </FormFieldItem>
+          <FormFieldItem
+            required
+            select
+            bp={{ xs: 5.5, sm: 3.5 }}
+            label='Prioridad'
+            name='code_pri'
+            defaultValue={dataRequest.code_pri}
           >
-            <TextFieldFullWidth
-              label="Empresa"
-              name="company_id"
-              value={dataRequest.company_tradename}
-              variant="outlined"
-              InputProps={{
-                readOnly: true,
-                endAdornment: (
-                  <InputAdornment position='end'>
-                    <CheckCircleOutlineIcon />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid
-            item
-            xs={12}
-          >
-            <TextFieldFullWidth
-              id="outlined-basic"
-              label="Area"
-              name="department"
-              value={dataRequest.department}
-              variant="outlined"
-              InputProps={{
-                readOnly: true,
-                endAdornment: (
-                  <InputAdornment position='end'>
-                    <CheckCircleOutlineIcon />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid
-            item
-            xs={12}
-          >
-            <TextFieldFullWidth
-              css={{
-                "& textarea" : {
-                  // minHeight: 200,
-                  height: 'fit-content',
-                }
-              }}
-              id="outlined-basic"
-              label="Razon"
-              name="reason"
-              value={dataRequest.reason}
-              variant="outlined"
-              multiline={true}
-              InputProps={{
-                readOnly: true,
-                maxLength: 500,
-                endAdornment: (
-                  <InputAdornment position='end'>
-                    <CheckCircleOutlineIcon />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid
-            item
-            xs={12}
-          >
-            <TextFieldFullWidth
-              css={{
-                "& textarea" : {
-                  minHeight: 200,
-                }
-              }}
-              required
-              id="outlined-basic"
-              label="Descripcion"
-              name="description"
-              variant="outlined"
-              multiline={true}
-              InputProps={{
-                maxLength: 500,
-              }}
-            />
-          </Grid>
+            <MenuItem key='Alta' value='1'>Baja</MenuItem>
+            <MenuItem key='Normal' value='2'>Normal</MenuItem>
+            <MenuItem key='Baja' value='3'>Alta</MenuItem>
+          </FormFieldItem>
+          <FormFieldItem
+            disabled
+            bp={{ xs: 12, sm: 3.5 }}
+            label='Empresa'
+            name='company_id'
+            value={dataRequest.company_tradename}
+          />
+          <FormFieldItem
+            disabled
+            bp={{ xs: 12, md: 5.5 }}
+            label='Area'
+            name='department'
+            value={dataRequest.department}
+          />
+          <FormFieldItem
+            disabled
+            bp={{ xs: 12, md: 5.5 }}
+            label='Solicitante'
+            name='solicitante'
+            value={dataRequest.user_id}
+          />
+          <FormFieldItem
+            disabled
+            bp={{ xs: 12 }}
+            label='Asunto'
+            name='subjects'
+            value={dataRequest.subject}
+          />
+          <FormFieldItem
+            disabled
+            bp={{ xs: 12 }}
+            label='Razon'
+            name='reason'
+            value={dataRequest.reason}
+            lines={{ multiline: true, maxLength: 500 }}
+          />
+          <FormFieldItem
+            css={{ background: '#fff' }}
+            bp={{ xs: 12 }}
+            label='Descripcion'
+            name='description'
+            lines={{ multiline: true, maxLength: 500 }}
+            onChange={e => setCounter(e.target.value.length)}
+            renderIcon={<LetterCounter current={counter} total={500}/>}
+          />
           <Grid item xs={12} sx={{ display: 'flex', justifyContent:' flex-end', gap: 1 }}>
             <ButtonModal
               type="submit"
