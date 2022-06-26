bc0f04e5ab0b805c05b540471f0eedb32a155a02
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-06-25 21:47:43 -0500

    Feat(pages/dashboard/HeaderSection/index.js): update

--- pm_frontend/src/pages/dashboard/HeaderSection/index.js
+++ pm_frontend/src/pages/dashboard/HeaderSection/index.js
@@ -3,17 +3,49 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import { useBackend } from 'hooks/useBackend';
 import CardState from 'pages/dashboard/CardState';
+import Graphic from 'pages/dashboard/Graphic';
+import ViewDatePicker from 'pages/dashboard/ViewDatePicker';
+import { flexbox } from '@mui/system';
 
 export default function Dashboard() {
   const [dashboard, setDashboard] = useState([]);
   const { getDashboard } = useBackend();
   const filters = {year:2022, month:6}
+
   useEffect(() => {
    getDashboard(filters).then(setDashboard);
   }, [getDashboard]);
   
+  const sasboard = [
+    {
+        bussines: "Autrisa",
+        number_sol: 1,
+        number_req: 2,
+        number_pro: 3,  
+        color: "red",
+    },
+    {
+        bussines: "NovaAutos",
+        number_sol: 4,
+        number_req: 5,
+        number_pro: 6,  
+        color: "blue",
+    },
+    {
+        bussines: "IncaMotors",
+        number_sol: 7,
+        number_req: 8,
+        number_pro: 9,  
+        color: "black",
+    },
+  ]
+
   return (
     <Box>
+      <h1>
+        Dashboard
+        <ViewDatePicker/>
+      </h1>
       <Grid container
       sx={{
         gap: 2,
@@ -25,6 +57,11 @@ export default function Dashboard() {
             {...obj}
             />
           ))
+        }
+      </Grid>
+      <Grid container>
+        {
+          <Graphic dashboard={sasboard}/>
         }
       </Grid>
     </Box>
