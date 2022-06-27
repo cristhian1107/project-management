41abf162b57517c7b1ada041668fc62088dd146a
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-06-26 22:54:06 -0500

    Feat(pm_frontend/src/pages/dashboard/HeaderSection/index.js): update

--- pm_frontend/src/pages/dashboard/HeaderSection/index.js
+++ pm_frontend/src/pages/dashboard/HeaderSection/index.js
@@ -3,48 +3,64 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import { useBackend } from 'hooks/useBackend';
 import CardState from 'pages/dashboard/CardState';
+import TextField from '@mui/material/TextField';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { es } from "date-fns/locale";
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Stack from '@mui/material/Stack';
 import Graphic from 'pages/dashboard/Graphic';
-import ViewDatePicker from 'pages/dashboard/ViewDatePicker';
-import { flexbox } from '@mui/system';
+import RadarStatus from 'pages/dashboard/RadarStatus';
 
 export default function Dashboard() {
   const [dashboard, setDashboard] = useState([]);
   const { getDashboard } = useBackend();
-  const filters = {year:2022, month:6}
+  const [value, setValue] = useState(new Date());
+  let filters = {
+    year: new Date().getFullYear(),
+    month: new Date().getMonth() + 1,
+  }
 
   useEffect(() => {
    getDashboard(filters).then(setDashboard);
   }, [getDashboard]);
   
-  const sasboard = [
-    {
-        bussines: "Autrisa",
-        number_sol: 1,
-        number_req: 2,
-        number_pro: 3,  
-        color: "red",
-    },
-    {
-        bussines: "NovaAutos",
-        number_sol: 4,
-        number_req: 5,
-        number_pro: 6,  
-        color: "blue",
-    },
-    {
-        bussines: "IncaMotors",
-        number_sol: 7,
-        number_req: 8,
-        number_pro: 9,  
-        color: "black",
-    },
-  ]
-
+  const handleDate = (newDate) => {
+    filters.year = newDate.getFullYear();
+    filters.month = newDate.getMonth() + 1;
+    getDashboard(filters).then(setDashboard);
+  };
+  
   return (
     <Box>
-      <h1>
-        Dashboard
-        <ViewDatePicker/>
+      <h1> Dashboard
+        <LocalizationProvider dateAdapter={AdapterDateFns} locale={es}>
+          <Stack spacing={3}
+            sx={{
+              display: "flex",
+              alignItems: "end",
+            }}
+          >
+            <DatePicker
+              views={['year', 'month']}
+              label="Año y mes"
+              minDate={new Date('2022-01-02')}
+              maxDate={new Date()}
+              value={value}
+              onChange = {(newValue) => {
+                setValue(newValue);
+                handleDate(newValue);
+              }}
+              renderInput={(params) => <TextField {...params} helperText={null}
+              sx={{
+                display: "flex",
+                width: "300px",
+                justifyContent: "end"
+              }}
+              />}
+           />
+          </Stack>
+        </LocalizationProvider>
       </h1>
       <Grid container
       sx={{
@@ -61,10 +77,12 @@ export default function Dashboard() {
       </Grid>
       <Grid container>
         {
-          <Graphic dashboard={sasboard}/>
+          dashboard[1] && <Graphic dashboard={dashboard[1]}/>
+        }
+        {
+          dashboard[2] && <RadarStatus dashboard={dashboard[2]}/>
         }
       </Grid>
     </Box>
   );
 }
-
