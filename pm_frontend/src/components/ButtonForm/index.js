b69103f0a66c2cd91cf509498e8be5a57cd1a037
MiguelBarreraDev <barreram987@gmail.com>
2022-06-28 23:02:36 -0500

    Add (frontend): css property to handle styles

--- pm_frontend/src/components/ButtonForm/index.js
+++ pm_frontend/src/components/ButtonForm/index.js
@@ -7,7 +7,7 @@ export default function ButtonForm ({ children, variant, sx, ...props }) {
         fontWeight: 'bold',
         color: '#FFFc',
         my: 1,
-
+        px: 4,
         background: `var(--${variant}-gradient)`,
         '&:hover': {
           boxShadow: () => `0 3px 10px var(--${variant}-primary)`,
