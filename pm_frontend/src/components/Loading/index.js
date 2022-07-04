24fa35a147aa814db6df75134227a29f66395950
MiguelBarreraDev <barreram987@gmail.com>
2022-07-04 01:34:15 -0500

    Add (frontend): Components to show spinner

--- pm_frontend/src/components/Loading/index.js
+++ pm_frontend/src/components/Loading/index.js
@@ -1,26 +1,2 @@
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
-
-export default function Loading () {
-
-  return (
-    <Box
-      sx={{
-        overflow: 'hidden',
-        background: 'var(--box-primary)',
-        width: '100%',
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        color: '#fff',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: '1.5rem',
-        gap: 2,
-      }}
-    >
-      Cargando
-      <CircularProgress disableShrink />
-    </Box>
-  );
-}
+export * from 'components/Loading/LoadingPage';
+export * from 'components/Loading/LoadingButton';
